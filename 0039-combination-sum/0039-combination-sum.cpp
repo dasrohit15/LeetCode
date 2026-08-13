aa536867441eb7098c1bf215c5b2251a64718a73
Rohit Das <dasrohit1636@gmail.com>
2026-08-13 16:35:05 +0530

    Time: 0 ms (100%), Space: 14 MB (77.39%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void findCombination(int idx, int tar, vector<int> &arr, vector<vector<int>> &ans, vector<int>&ds){
+    /*void findCombination(int idx, int tar, vector<int> &arr, vector<vector<int>> &ans, vector<int>&ds){
         if(idx == arr.size()){
             if(tar == 0){
                 ans.push_back(ds);
@@ -14,9 +14,31 @@ public:
             ds.pop_back();
         }
         findCombination(idx + 1, tar, arr, ans, ds);
+    }*/
+    void findCombination(int idx, int tar, vector<int>& arr,
+                          vector<vector<int>>& ans, vector<int>& ds) {
+
+        if (tar == 0) {
+            ans.push_back(ds);
+            return;
+        }
+
+        for (int i = idx; i < arr.size(); i++) {
+
+            if (arr[i] > tar)
+                break;
+
+            ds.push_back(arr[i]);
+
+            // Use i again because the same number can be reused
+            findCombination(i, tar - arr[i], arr, ans, ds);
+
+            ds.pop_back();
+        }
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
         vector<vector<int>>ans;
         vector<int>ds;
         findCombination(0, target, candidates, ans, ds);
