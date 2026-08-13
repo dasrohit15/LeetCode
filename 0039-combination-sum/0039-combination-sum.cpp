feb5463d8294211aef84a9490ce1d7ff5a3338d4
Rohit Das <dasrohit1636@gmail.com>
2026-08-13 13:34:40 +0530

    Time: 0 ms (100%), Space: 14 MB (94.1%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void findCombination(int idx, int tar, vector<int> &arr, vector<vector<int>> &ans, vector<int>&ds){
+        if(idx == arr.size()){
+            if(tar == 0){
+                ans.push_back(ds);
+            }
+            return;
+        }
+        
+        if(arr[idx] <= tar){
+            ds.push_back(arr[idx]);
+            findCombination(idx, tar - arr[idx], arr, ans, ds);
+            ds.pop_back();
+        }
+        findCombination(idx + 1, tar, arr, ans, ds);
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        findCombination(0, target, candidates, ans, ds);
+        return ans;   
+    }
+};
