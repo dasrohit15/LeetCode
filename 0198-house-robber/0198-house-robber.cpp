b67de5f92aad3cbf4a20977787dba07193a22ead
Rohit Das <dasrohit1636@gmail.com>
2026-08-18 17:13:00 +0530

    Time: 0 ms (100%), Space: 10.8 MB (6.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    /*int func(int idx, vector<int> &nums, vector<int> &dp){
+    int func(int idx, vector<int> &nums, vector<int> &dp){
         if(idx == 0) return nums[0];
         if(idx < 0) return 0;
         if(dp[idx] != -1)  return dp[idx];
         int take = nums[idx] + func(idx - 2, nums, dp);
         int not_take = func(idx - 1, nums, dp);
         return dp[idx] = max(take, not_take);
-    }*/
-    int func(int idx, vector<int> &nums, vector<int> &dp){
+    }
+    /*int func(int idx, vector<int> &nums, vector<int> &dp){
         dp[0] = nums[0];
         for(int i = 1; i <= idx; i++){
             int take = nums[i] ;
@@ -17,7 +17,7 @@ public:
             dp[i] = max(take, not_take);
         }
         return dp[idx];
-    }
+    }*/
     
     int rob(vector<int>& nums) {
         int n = nums.size();
