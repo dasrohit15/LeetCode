5cb53733ddea45e900d62e9704c23c832e0f8dcb
Rohit Das <dasrohit1636@gmail.com>
2026-08-19 18:03:18 +0530

    Time: 4 ms (15.34%), Space: 16.3 MB (42.77%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int func(int i, int j,vector<vector<int>>& arr, vector<vector<int>>& dp ){
         if(i==0 && j==0) return arr[0][0];
-        if(i<0 || j<0) return 999999;
+        if(i<0 || j<0) return 1e9;
         if(dp[i][j] != -1) return dp[i][j];
         int up = arr[i][j] + func(i-1,j,arr,dp);
         int left = arr[i][j] + func(i,j-1,arr,dp);
