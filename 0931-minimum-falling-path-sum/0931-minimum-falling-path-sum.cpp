b6c0cd89d6bb2219a7a5ee7281738a02423f3e14
Rohit Das <dasrohit1636@gmail.com>
2026-08-20 00:56:18 +0530

    Time: 2 ms (44.53%), Space: 15 MB (67.57%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -23,4 +23,26 @@ public:
         }
         return minm;
     }
+
+    /*//-------Memoization(top-down approach)
+    int func(int i, int j, vector<vector<int>>& arr, vector<vector<int>>& dp){
+        if(j<0 || j>=arr[0].size()) return 1e9;
+        if(i == 0) return arr[0][j];
+        if(dp[i][j] != -1) return dp[i][j];
+        int up = arr[i][j] + func(i-1, j, arr, dp);
+        int ld = arr[i][j] + func(i-1, j-1, arr, dp);
+        int rd = arr[i][j] + func(i-1, j+1, arr, dp);
+        return dp[i][j] = min(up, min(ld,rd));
+    }
+    int minFallingPathSum(vector<vector<int>>& matrix){
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        int mini = 1e9;
+        for(int j = 0; j < m ;j++){
+            mini = min(mini, func(n-1,j,matrix,dp));
+        }
+        return mini;
+    }*/
+
 };
