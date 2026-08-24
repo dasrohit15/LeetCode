074491b3e974c0fbb5c937e8fe5f52ef51e2a6b9
Rohit Das <dasrohit1636@gmail.com>
2026-08-24 15:18:27 +0530

    Time: 8 ms (60.34%), Space: 15.5 MB (45.32%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    int func(int idx,int tar,vector<int>&arr){
+    int func(int idx,int tar,vector<int>&arr,vector<vector<int>>& dp){
         if(idx==0){
             if(tar==0 && arr[0]==0)return 2;
             if(tar==0 || arr[0]==tar)return 1;
             return 0;
         }
-        int not_take = func(idx-1,tar,arr);
+        if(dp[idx][tar]!=-1) return dp[idx][tar];
+        int not_take = func(idx-1,tar,arr,dp);
         int take = 0;
-        if(arr[idx]<=tar) take = func(idx-1,tar-arr[idx],arr);
-        return take + not_take;
+        if(arr[idx]<=tar) take = func(idx-1,tar-arr[idx],arr,dp);
+        return dp[idx][tar] = take + not_take;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
@@ -17,6 +18,7 @@ public:
         for(int i = 0;i<n;i++) totSum += nums[i];
         if((totSum - target)<0 || (totSum-target)%2) return 0;
         int k = (totSum-target)/2;
-        return func(n-1,k,nums);
+        vector<vector<int>>dp(n,vector<int>(k+1,-1));
+        return func(n-1,k,nums,dp);
     }
 };
