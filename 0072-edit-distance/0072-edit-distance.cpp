bd974407ac8ecec3a7d377d9d3db63f9fc7cfb39
Rohit Das <dasrohit1636@gmail.com>
2026-08-25 19:17:24 +0530

    Time: 7 ms (60.87%), Space: 13.2 MB (61.23%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int func(int n, int m, string &s, string &t,vector<vector<int>>& dp){
+    /*int func(int n, int m, string &s, string &t,vector<vector<int>>& dp){
         //base case
         if(n==0) return m;
         if(m==0)return n;
@@ -18,6 +18,30 @@ public:
         int n=word1.length();
         int m=word2.length();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return func(n,m,word1,word2,dp);
+    }*/
+
+    int func(int n, int m, string &s, string &t,vector<vector<int>>& dp){
+        //base case
+        for(int j=0;j<=m;j++) dp[0][j]= j;
+        for(int i=0;i<=n;i++) dp[i][0]= i;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]) dp[i][j] = dp[i-1][j-1];
+                else{
+                    dp[i][j] = min(1+dp[i][j-1] ,
+                                    min(1+dp[i-1][j] , 1+dp[i-1][j-1])
+                                );
+                }
+            }
+        } 
+        return dp[n][m]; 
+    }
+
+    int minDistance(string word1, string word2) {
+        int n=word1.length();
+        int m=word2.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         return func(n,m,word1,word2,dp);
     }
 };
