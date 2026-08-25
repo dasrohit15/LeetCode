beef8aa515167a87b1f930a21c9917bc18f04a89
Rohit Das <dasrohit1636@gmail.com>
2026-08-25 18:40:26 +0530

    Time: 4 ms (81.29%), Space: 13.2 MB (61.23%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int func(int n, int m, string &s, string &t,vector<vector<int>>& dp){
         //base case
-        if(n<0) return m+1;
-        if(m<0)return n+1;
+        if(n==0) return m;
+        if(m==0)return n;
         if(dp[n][m]!=-1)return dp[n][m];
         //matched
-        if(s[n]==t[m]) return dp[n][m] = func(n-1,m-1,s,t,dp);
+        if(s[n-1]==t[m-1]) return dp[n][m] = func(n-1,m-1,s,t,dp);
         else{
             return dp[n][m] = min(1+func(n,m-1,s,t,dp) ,
                                     min(1+func(n-1,m,s,t,dp) , 1+func(n-1,m-1,s,t,dp))
@@ -17,7 +17,7 @@ public:
     int minDistance(string word1, string word2) {
         int n=word1.length();
         int m=word2.length();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return func(n-1,m-1,word1,word2,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return func(n,m,word1,word2,dp);
     }
 };
