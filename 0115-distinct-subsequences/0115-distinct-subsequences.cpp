bf9ada8d9058920b3251e1916f88e47f7bd82ebe
Rohit Das <dasrohit1636@gmail.com>
2026-08-25 17:53:20 +0530

    Time: 33 ms (39.43%), Space: 44.2 MB (31.62%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int func(int n,int m,string &s,string &t,vector<vector<int>>&dp){
+    /*int func(int n,int m,string &s,string &t,vector<vector<int>>&dp){
         //if 't' get exhausted i.e. found match in s
         if(m==0)return 1;
         //if 's' get exhausted i.e. not found match in s
@@ -15,22 +15,24 @@ public:
         int m = t.length();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
         return func(n,m,s,t,dp);
-    }
+    }*/
 
-    /*int func(int n,int m,string &s,string &t,vector<vector<int>>&dp){
-        //if 't' get exhausted i.e. found match in s
-        if(m==0)return 1;
-        //if 's' get exhausted i.e. not found match in s
-        if(n==0)return 0;
-        if(dp[n][m]!=-1)return dp[n][m];
-        if(s[n-1]==t[m-1])  dp[n][m] = dp[n-1][m-1] + dp[n-1][m];
-        else dp[n][m] = dp[n-1][m];
+    int func(int n,int m,string &s,string &t,vector<vector<double>>&dp){
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+        for(int j=1;j<=m;j++) dp[0][j]=0;
+        for(int idx1=1;idx1<=n;idx1++){
+            for(int idx2=1;idx2<=m;idx2++){
+                if(s[idx1-1]==t[idx2-1]) dp[idx1][idx2] = dp[idx1-1][idx2-1] + dp[idx1-1][idx2];
+                else dp[idx1][idx2] = dp[idx1-1][idx2];
+            }
+        }
+        return (int)dp[n][m];
     }
 
     int numDistinct(string s, string t) {
         int n = s.length();
         int m = t.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
         return func(n,m,s,t,dp);
-    }*/
+    }
 };
