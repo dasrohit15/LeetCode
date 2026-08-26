18f8fec32a4e9bd7b25dddbb5dff4d8315b8f40a
Rohit Das <dasrohit1636@gmail.com>
2026-08-26 12:59:33 +0530

    Time: 51 ms (54.58%), Space: 13.9 MB (77.65%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool func(int i, int j, string &s,string&p, vector<vector<int>>&dp){
+    /*bool func(int i, int j, string &s,string&p, vector<vector<int>>&dp){
         //string p and s both get exhausted
         if(i==0 & j==0)return true;
         //if p get exhausted and there is something still left in s
@@ -21,6 +21,36 @@ public:
         int n = p.length();
         int m = s.length();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return func(n,m,s,p,dp);
+    }*/
+
+    bool func(int n,int m,string &s,string &p,vector<vector<bool>> &dp){
+        dp[0][0]=true;
+        for(int j=1;j<=m;j++) dp[0][j]=false;
+        for(int i=1;i<=n;i++){
+            bool flag =true;
+            for(int x=1;x<=i;x++){
+                if(p[x-1]!='*'){
+                    flag = false;
+                    break;
+                }
+            }
+            dp[i][0]= flag;
+        }
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(p[i-1]==s[j-1]  || p[i-1]=='?') dp[i][j]=dp[i-1][j-1];
+                else if(p[i-1]=='*')  dp[i][j]=dp[i-1][j] || dp[i][j-1];
+                else dp[i][j]= false;
+            }
+        }
+        return dp[n][m];
+    }
+
+    bool isMatch(string s, string p) {
+        int n = p.length();
+        int m = s.length();
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
         return func(n,m,s,p,dp);
     }
 };
