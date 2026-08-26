789a255a6b7bf8bb4d217cff97b6ba1669426a00
Rohit Das <dasrohit1636@gmail.com>
2026-08-26 15:22:53 +0530

    Time: 1589 ms (25.98%), Space: 30.5 MB (20.49%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -11,11 +11,11 @@ public:
         return true;
     }
 
-    int func(int i,int n, string &s, vector<int> &dp){
+    /*int func(int i,int n, string &s, vector<int> &dp){
         if(i==n)return 0;
+        if(dp[i]!=-1) return dp[i];
         string temp = "";
-        int minCost = INT_MAX; 
-        if(dp[i]!=-1) return dp[i]; 
+        int minCost = INT_MAX;  
         for(int j=i;j<n;j++){
             temp += s[j];
             if(isPalindrome(temp)){
@@ -29,6 +29,29 @@ public:
     int minCut(string s) {
         int n = s.length();
         vector<int>dp(n,-1);
+        return func(0,n,s,dp) - 1;
+    }*/
+
+    int func(int i,int n,string &s,vector<int> &dp){
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            string temp ="";
+            int minCost = INT_MAX;
+            for(int j =i;j<n;j++){
+                temp += s[j];
+                if(isPalindrome(temp)){
+                    int cost = 1 + dp[j+1];
+                    minCost = min(minCost,cost);
+                }
+            }
+            dp[i]=minCost;
+        }
+        return dp[0];
+    }
+
+    int minCut(string s) {
+        int n = s.length();
+        vector<int>dp(n+1,0);
         return func(0,n,s,dp) - 1;
     }
 };
