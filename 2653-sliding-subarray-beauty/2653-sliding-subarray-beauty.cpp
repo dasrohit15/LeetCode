8f830d1a427fa83d7b6e6b4a7e40fa2cc5043aef
Rohit Das <dasrohit1636@gmail.com>
2026-08-31 23:21:07 +0530

    Time: 267 ms (12.96%), Space: 191.3 MB (23.07%) - LeetHub

---
+++ 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findxth(map<int,int>&mp,int x){
+        int count=0;
+        for(auto it:mp){
+            count+=it.second;
+            if(count>=x)return it.first;
+        }
+        return 0;
+    }
+
+    vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
+        int n=nums.size();
+        vector<int>ans;
+        map<int,int>mpp;
+        int l=0;
+        for(int r=0;r<n;r++){
+            if(nums[r]<0) mpp[nums[r]]++;
+            if(r-l+1 < k)continue;
+            else if(r-l+1 ==k){
+                int xth_ele = findxth(mpp,x);
+                ans.push_back(xth_ele);
+                mpp[nums[l]]--;
+                if(mpp[nums[l]]==0)mpp.erase(nums[l]);
+                l++;
+            }
+        }
+        return ans;
+    }
+};
