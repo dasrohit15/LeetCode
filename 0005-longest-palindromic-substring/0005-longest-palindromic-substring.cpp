e3d29e339dff8accdbc73b3394aeab3b757bd2bd
Rohit Das <dasrohit1636@gmail.com>
2026-09-17 16:19:53 +0530

    Time: 3 ms (98.12%), Space: 9.4 MB (57.97%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,75 +1,26 @@
 class Solution {
 public:
     int expand(string &s, int left, int right) {
-
-            while (left >= 0 && right < s.size() && s[left] == s[right]) {
-                left--;
-                right++;
-            }
-
-            return right - left - 1;
-    }
-    
-    string longestPalindrome(string s) {
-        /*int n = s.size();
-
-        int start = 0;
-        int maxLen = 1;
-
-        for (int i = 0; i < n; i++) {
-
-            // Odd length palindrome
-            int left = i;
-            int right = i;
-
-            while (left >= 0 && right < n && s[left] == s[right]) {
-                if (right - left + 1 > maxLen) {
-                    start = left;
-                    maxLen = right - left + 1;
-                }
-
-                left--;
-                right++;
-            }
-
-            // Even length palindrome
-            left = i;
-            right = i + 1;
-
-            while (left >= 0 && right < n && s[left] == s[right]) {
-                if (right - left + 1 > maxLen) {
-                    start = left;
-                    maxLen = right - left + 1;
-                }
-
-                left--;
-                right++;
-            }
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            left--;
+            right++;
         }
+        return right - left - 1;
+    }
 
-        return s.substr(start, maxLen);*/
+    string longestPalindrome(string s) {
         int start = 0;
-        int maxLen = 0;
+        int end = 0;
 
         for (int i = 0; i < s.size(); i++) {
-
-            // Odd length palindrome
-            int len1 = expand(s, i, i);
-
-            // Even length palindrome
-            int len2 = expand(s, i, i + 1);
-
+            int len1 = expand(s, i, i);              // Odd length palindrome
+            int len2 = expand(s, i, i + 1);          // Even length palindrome
             int len = max(len1, len2);
-
-            if (len > maxLen) {
-                maxLen = len;
-
-                // Find starting index
-                start = i - (len - 1) / 2;
+            if (len > end-start+1) {
+                end = i + len/2;
+                start = i - (len - 1) / 2;          // Find starting index
             }
         }
-
-        return s.substr(start, maxLen);
-        
+        return s.substr(start, end-start+1);        // in C++: s.substr(start,length)
     }
 };
